b0f49ee6f56f210cc71dc186b67c8fdc38996c84
ShuvroSankar <sankarshuvro1010@gmail.com>
2023-11-26 03:15:07 +0600

    Create front end

---
+++ frontend/src/products.js
@@ -0,0 +1,110 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Airpods Wireless  Headphones',
+        image: '/images/airpods.jpg',
+        description: 'Bluetooth is a short-range wireless technology standard that is used for exchanging data between fixed and mobile devices over short distances and building personal area networks. In the most widely used mode, transmission power is limited to 2.5 milliwatts, giving it a very short range of up to 10 metres.',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 0,
+        rating: 4.5,
+        numReviews: 12,
+
+    },
+    {
+        _id: '2',
+        name: 'UltraBook Pro X1',
+        image: '/images/alexa.jpg',
+        description: 'Powerful and lightweight Ultrabook with SSD storage for fast performance. Ideal for professionals and students on the go.',
+        brand: 'Lenovo',
+        category: 'Electronics',
+        price: 1299.99,
+        countInStock: 15,
+        rating: 4.7,
+        numReviews: 20,
+    },
+    {
+        _id: '3',
+        name: 'SmartGuard Pro Security Camera',
+        image: '/images/camera.jpg',
+        description: 'Keep your home secure with the latest SmartGuard Pro Security Camera. HD video, night vision, and smart integration for a peace of mind.',
+        brand: 'SmartGuard',
+        category: 'Electronics',
+        price: 149.99,
+        countInStock: 8,
+        rating: 4.2,
+        numReviews: 10,
+    },
+    {
+        _id: '4',
+        name: 'GamerXtreme RGB Edition Laptop',
+        image: '/images/mouse.jpg',
+        description: 'Experience gaming like never before with the GamerXtreme RGB Edition Laptop. High-performance specs and customizable RGB keyboard for immersive gameplay.',
+        brand: 'GamerTech',
+        category: 'Electronics',
+        price: 1699.99,
+        countInStock: 5,
+        rating: 4.9,
+        numReviews: 25,
+    },
+    {
+        _id: '5',
+        name: 'UltraVision 4K UHD Smart TV',
+        image: '/images/phone.jpg',
+        description: 'Bring the cinema experience to your living room with the UltraVision 4K UHD Smart TV. Smart features, stunning visuals, and immersive audio.',
+        brand: 'UltraTech',
+        category: 'Electronics',
+        price: 799.99,
+        countInStock: 12,
+        rating: 4.6,
+        numReviews: 18,
+    },
+    {
+        _id: '6',
+        name: 'StealthGamer Wireless Mouse',
+        image: '/images/playstation.jpg',
+        description: 'Enhance your gaming setup with the StealthGamer Wireless Gaming Mouse. Precise controls, customizable buttons, and ergonomic design for optimal performance.',
+        brand: 'StealthTech',
+        category: 'Electronics',
+        price: 49.99,
+        countInStock: 20,
+        rating: 4.4,
+        numReviews: 15,
+    },
+    {
+        _id: '7',
+        name: 'FitPro Plus Bluetooth Fitness',
+        image: '/images/sample.jpg',
+        description: 'Achieve your fitness goals with the FitPro Plus Bluetooth Fitness Tracker. Monitor your activity, heart rate, and receive smart notifications on the go.',
+        brand: 'FitPro',
+        category: 'Wearable Tech',
+        price: 79.99,
+        countInStock: 18,
+        rating: 4.8,
+        numReviews: 22,
+    },
+    {
+        _id: '8',
+        name: 'SwiftDrive Portable External SSD',
+        image: '/images/screens.png',
+        description: 'Transfer files in seconds with the SwiftDrive Portable External SSD. Compact design, high-speed data transfer, and ample storage capacity.',
+        brand: 'SwiftTech',
+        category: 'Storage Devices',
+        price: 129.99,
+        countInStock: 14,
+        rating: 4.5,
+        numReviews: 16,
+    },
+]
+export default products
+
+
+
+
+
+
+
+
+
+

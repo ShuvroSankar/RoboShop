dd3a212c3a6d5d7d9ddd3bce8e39eed7f103bf78
ShuvroSankar <sankarshuvro1010@gmail.com>
2023-11-30 20:07:06 +0600

    redux react-redux functionlity implementation

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
 import Products from "../components/Products";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductsQuery } from "../slices/productApiSlice";
+
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading, error } = useGetProductsQuery(); // Add parentheses here
+
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4}>
-            <Products product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message varient="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4}>
+                <Products product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };

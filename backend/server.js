3529c5124aca9e39eb9fc9096f15651d5d90350c
ShuvroSankar <sankarshuvro1010@gmail.com>
2023-12-03 01:58:55 +0600

    back end authentication setup

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,26 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import cookieParser from 'cookie-parser';
 const port = process.env.PORT || 5000
 
 connectDB() // connect to mongoDB
 const app = express()
+//  Body parser middlewire
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//  Cookie parser middlewire
+app.use(cookieParser())
+
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 

ca2f2f9ef3c7df9b830519fffb107cf9f5fa6ff9
ShuvroSankar <sankarshuvro1010@gmail.com>
2023-12-05 01:28:45 +0600

    Private Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,14 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import reportWebVitals from './reportWebVitals';
 import App from './App'
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
@@ -22,7 +24,9 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
-      <Route path='/shipping' element={<ShippingScreen />} />
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 )
